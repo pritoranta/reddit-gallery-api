94b591aa1712340b47222880dd0c5fc485ef73e5
P Ritoranta <p.ritoranta@proton.me>
2025-09-02 23:32:22 +0300

    feat: add cors

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 	router.SetTrustedProxies(nil)
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"https://pritoranta.net", "http://localhost:5173"},
+	}))
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, "pong")
 	})
